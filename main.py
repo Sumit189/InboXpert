bdc223a7010dba17e7fc06d7b39af4367c8ecdf5
Sumit Paul <sumit.18.paul@gmail.com>
2025-02-10 23:47:39 +0530

    remove comment test

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# Gmail API Scopes
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify", "https://www.googleapis.com/auth/gmail.readonly", "https://mail.google.com/"]
 
 def authenticate_gmail():
